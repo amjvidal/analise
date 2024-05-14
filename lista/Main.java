6f7113771a57b99e10302b4c0497b2fcab7dbf0f
amjvidal <136771834+amjvidal@users.noreply.github.com>
2024-05-13 21:45:16 -0300

    Adicionando comentários no Main.java

--- lista/Main.java
+++ lista/Main.java
@@ -10,12 +10,27 @@ public class Main{
         String path = "C:\\Users\\anton\\OneDrive\\Documentos\\VCS\\teste\\rsc\\arqnovo.txt";
         
         try {
+
             //lendo o arquivo
+
             BufferedReader reader = new BufferedReader(new FileReader(path));
             String linha;
+
+            // contagem de linhas
+
             int contagemLinhas = 1;
+
+            // confirmação de leitura do arquivo
+
             System.out.println("Arquivo sendo Lido");
+
+            // while que vai percorrer todas as linhas até acabar o arquivo
+
             while ((linha = reader.readLine()) != null) {
+
+                // adiciona todos os numeros da primeira linhas em um array de strings 
+                // e converte de string para int e adiciona os numeros na lista
+                
                 if (contagemLinhas == 1) { 
                     String[] valores = linha.split(" ");
                     System.out.println("Lista sem alterações: ");
@@ -30,18 +45,21 @@ public class Main{
                             lista.adicionar(Integer.parseInt(valor)); 
                         }
                     }
+
+                    // para todas as linhas apos a primeira o codigo verifica o operador para realizar a operação
+
                 } else if (contagemLinhas > 1) { 
                     String[] partes = linha.split(" ");
-                    char acao = partes[0].charAt(0);
-                    if (acao == 'P') {
+                    char operador = partes[0].charAt(0);
+                    if (operador == 'P') {
                         System.out.println("Operação: " + linha);
                         lista.print();
-                    } else if (acao == 'A') {
+                    } else if (operador == 'A') {
                         System.out.println("Operação: " + linha);
                         int numero = Integer.parseInt(partes[1]);
                         int posicao = Integer.parseInt(partes[2]);
                         lista.inserePosicao(numero, posicao); 
-                    } else if (acao == 'R'){
+                    } else if (operador == 'R'){
                         System.out.println("Operação: " + linha);
                         int posicao = Integer.parseInt(partes[1]);
                         lista.remove(posicao); 
@@ -49,8 +67,12 @@ public class Main{
                 }
                 contagemLinhas++;
             }
+
+            // fechando o leitor de arquivos
+
             reader.close();
         } catch (IOException e) {
+            System.out.println("Erro");
             e.printStackTrace();
         }
         
